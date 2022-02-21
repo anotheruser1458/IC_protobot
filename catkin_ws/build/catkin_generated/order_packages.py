1ea4644a170ea9140f5c576a7e4b20e54d81a24d
anotheruser1458 <maxwellpauly32@gmail.com>
2022-02-20 23:27:16 -1000

    build attempt

--- catkin_ws/build/catkin_generated/order_packages.py
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/maxwell/Desktop/IC_protobot/catkin_ws/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
+underlay_workspaces = '/home/maxwell/Desktop/IC_protobot/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/maxwell/Desktop/IC_protobot/catkin_ws/devel;/opt/ros/noetic' != '' else []
