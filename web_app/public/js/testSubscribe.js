e820f584416e83194b85459f43409886532e6f98
anotheruser1458 <maxwellpauly32@gmail.com>
2022-02-28 14:51:59 -1000

    react, gui package, launch file

--- web_app/public/js/testSubscribe.js
+++ web_app/public/js/testSubscribe.js
@@ -1,13 +1,13 @@
-var ros = new ROSLIB.Ros({
-    url:'ws://localhost:9090'
-})
+// var ros = new ROSLIB.Ros({
+//     url:'ws://localhost:9090'
+// })
 
-var text_listener = new ROSLIB.Topic({
-    ros:ros,
-    name:"/score",
-    messageType:"std_msgs/Int64"
-})
+// var text_listener = new ROSLIB.Topic({
+//     ros:ros,
+//     name:"/score",
+//     messageType:"std_msgs/Int64"
+// })
 
-text_listener.subscribe((m)=> {
-    document.getElementById("data").innerHTML = m.data
-})
+// text_listener.subscribe((m)=> {
+//     document.getElementById("data").innerHTML = m.data
+// })
