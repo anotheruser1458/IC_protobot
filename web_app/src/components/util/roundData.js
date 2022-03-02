62e1d66e3e360175624a949165caeaf9e1bc2f6f
anotheruser1458 <maxwellpauly32@gmail.com>
2022-03-01 20:58:02 -1000

    ignored demo videos

--- web_app/src/components/util/roundData.js
+++ web_app/src/components/util/roundData.js
@@ -2,5 +2,4 @@ function roundWithPrecision (n, precision) {
     const precisionWithPow10 = Math.pow(10, precision);
     return Math.round(n * precisionWithPow10) / precisionWithPow10;
 }
-
 export {roundWithPrecision}
